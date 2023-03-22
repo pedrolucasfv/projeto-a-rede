2ee02b478560e6b321f164b6817b9885b81a3796
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-03-22 10:49:53 -0300

    feat: add Oficinas page

---
+++ src/templates/Oficinas/index.tsx
@@ -0,0 +1,37 @@
+import OficinaContent from '../../components/OficinaContent'
+import Banner from '../../components/Banner'
+import Footer from '../../components/Footer'
+import Menu from '../../components/Menu'
+import * as S from './styles'
+
+const Oficinas = () => {
+  return (
+    <S.Wrapper>
+      <Menu />
+      <OficinaContent
+      name="Inglês para crianças"
+      description="Essa é uma oficina para ensinar inglês as crianças carentes para capacitá-las e promover um futuro melhor a elas. O inglês é de suma imporância e abre muitas portas para oportunidades de melhoria de vida."
+      image='/img/logo.png'
+      time="seg a sex: 18:00 até 19:00"
+    />
+    <OficinaContent
+      name="Inglês para crianças"
+      description="Essa é uma oficina para ensinar inglês as crianças carentes para capacitá-las e promover um futuro melhor a elas. O inglês é de suma imporância e abre muitas portas para oportunidades de melhoria de vida."
+      image='/img/logo.png'
+      time="seg a sex: 18:00 até 19:00"
+      invert
+    />
+    <OficinaContent
+      name="Inglês para crianças"
+      description="Essa é uma oficina para ensinar inglês as crianças carentes para capacitá-las e promover um futuro melhor a elas. O inglês é de suma imporância e abre muitas portas para oportunidades de melhoria de vida."
+      available={false}
+      image='/img/logo.png'
+      time="seg a sex: 18:00 até 19:00"
+      
+    />
+      <Footer />
+    </S.Wrapper>
+  )
+}
+
+export default Oficinas
---
+++ src/templates/Oficinas/styles.ts
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.main`
+    background: linear-gradient(
+      to top left,
+      #e3e3e3,
+      #dedede,
+      #e5e5e5,
+      #ececec,
+      #f5f5f5
+    );
+`;
+
+export const Menu = styled.div`
+  position: fixed;
+  width: 100%;
+  z-index: 100;
+`;
