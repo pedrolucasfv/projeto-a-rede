f8c6b3dce53c1c33fcb2caf12485327cf328badf
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-03-22 11:56:27 -0300

    feat: adicionando opção de cor do divider

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -2,10 +2,11 @@ import * as S from "./styles";
 
 export type DividerProps = {
   title: string;
+  color: "primary" | "secondary" | "tertiary" | "quaternary"
 };
 
-const Divider = ({ title }: DividerProps) => (
-  <S.Wrapper>
+const Divider = ({ title, color }: DividerProps) => (
+  <S.Wrapper color={color}>
     <S.Title>{title}</S.Title>
   </S.Wrapper>
 );
--- src/components/Divider/styles.ts
+++ src/components/Divider/styles.ts
@@ -1,24 +1,45 @@
-import styled, { css } from 'styled-components'
-import media from 'styled-media-query'
+import styled, { css } from "styled-components";
+import media from "styled-media-query";
 
-export const Wrapper = styled.main`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 10rem;
-`
+type WrapperProps = {
+  color: "primary" | "secondary" | "tertiary" | "quaternary";
+};
+
+export const Wrapper = styled.main<WrapperProps>`
+  ${({ color, theme }) => css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-top: 10rem;
+    ${color == "primary" &&
+    css`
+      color: ${theme.colors.primary};
+    `}
+    ${color == "secondary" &&
+    css`
+      color: ${theme.colors.secondary};
+    `}
+      ${color == "tertiary" &&
+    css`
+      color: ${theme.colors.tertiary};
+    `}
+      ${color == "quaternary" &&
+    css`
+      color: ${theme.colors.quaternary};
+    `}
+  `}
+`;
 
 export const Title = styled.h2`
   max-width: 65rem;
   text-align: center;
   font-size: 12rem;
-  font-weight: 800;
+  font-weight: 700;
   text-transform: uppercase;
 
-  
   margin-bottom: 8rem;
-  ${media.lessThan('medium')`
+  ${media.lessThan("medium")`
   margin-left: 5rem;
   font-size: 4rem;
   `}
-`
+`;
