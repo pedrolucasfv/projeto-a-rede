a9df04e1b963757f0b48f9e89b0dbd9999adb8ae
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-03-23 10:26:19 -0300

    feat: add animação do título do divider

--- src/components/Divider/index.tsx
+++ src/components/Divider/index.tsx
@@ -2,12 +2,15 @@ import * as S from "./styles";
 
 export type DividerProps = {
   title: string;
-  color: "primary" | "secondary" | "tertiary" | "quaternary"
+  color: "primary" | "secondary" | "tertiary" | "quaternary";
 };
 
 const Divider = ({ title, color }: DividerProps) => (
-  <S.Wrapper color={color}>
-    <S.Title>{title}</S.Title>
+  <S.Wrapper>
+    <S.Title color={color}>
+      <span>{title}</span>
+      <span>{title}</span>
+    </S.Title>
   </S.Wrapper>
 );
 
--- src/components/Divider/styles.ts
+++ src/components/Divider/styles.ts
@@ -1,45 +1,92 @@
+import theme from "../../styles/theme";
 import styled, { css } from "styled-components";
 import media from "styled-media-query";
 
-type WrapperProps = {
+type ColorProps = {
   color: "primary" | "secondary" | "tertiary" | "quaternary";
 };
 
-export const Wrapper = styled.main<WrapperProps>`
-  ${({ color, theme }) => css`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    margin-top: 10rem;
-    ${color == "primary" &&
-    css`
-      color: ${theme.colors.primary};
-    `}
-    ${color == "secondary" &&
-    css`
-      color: ${theme.colors.secondary};
-    `}
-      ${color == "tertiary" &&
-    css`
-      color: ${theme.colors.tertiary};
-    `}
-      ${color == "quaternary" &&
-    css`
-      color: ${theme.colors.quaternary};
-    `}
-  `}
+export const Wrapper = styled.main`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
 `;
 
-export const Title = styled.h2`
-  max-width: 65rem;
-  text-align: center;
-  font-size: 12rem;
-  font-weight: 700;
-  text-transform: uppercase;
+const modifiers = {
+  primary: () => css`
+    span:nth-child(2) {
+      color: ${theme.colors.primary};
+    }
+  `,
+  secondary: () => css`
+    span:nth-child(2) {
+      color: ${theme.colors.secondary};
+    }
+  `,
+  tertiary: () => css`
+    span:nth-child(2) {
+      color: ${theme.colors.tertiary};
+    }
+  `,
+  quaternary: () => css`
+    span:nth-child(2) {
+      color: ${theme.colors.quaternary};
+    }
+  `,
+};
 
-  margin-bottom: 8rem;
-  ${media.lessThan("medium")`
-  margin-left: 5rem;
-  font-size: 4rem;
+export const Title = styled.div<ColorProps>`
+  ${({ color, theme }) => css`
+    margin-top: 15rem;
+    font-weight: 700;
+    max-width: 65rem;
+    margin-bottom: 8rem;
+    position: relative;
+    span {
+      position: absolute;
+      color: #fff;
+      font-size: 12rem;
+      transform: translate(-50%, -50%);
+    }
+    span:nth-child(1) {
+      color: transparent;
+      -webkit-text-stroke: 0.1rem black;
+    }
+    span:nth-child(2) {
+      color: rgb(128, 198, 255);
+      -webkit-text-stroke: 1px #3bd9d9;
+      animation: animation 3s ease-in-out infinite;
+    }
+    @keyframes animation {
+      0%,
+      100% {
+        clip-path: polygon(
+          0% 45%,
+          15% 44%,
+          32% 50%,
+          54% 60%,
+          70% 61%,
+          84% 59%,
+          100% 52%,
+          100% 100%,
+          0% 100%
+        );
+      }
+      50% {
+        clip-path: polygon(
+          0% 60%,
+          16% 65%,
+          34% 66%,
+          51% 62%,
+          67% 50%,
+          84% 55%,
+          100% 46%,
+          100% 100%,
+          0% 100%
+        );
+      }
+    }
+    ${color && modifiers[color]()}
   `}
 `;
