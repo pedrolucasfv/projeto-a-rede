a63e226de5623a692dfdaf196ac5743fec6f0a68
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-03-26 10:03:03 -0300

    feat: reformulando o banner component

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,21 +1,55 @@
-import Divider from "../Divider";
 import * as S from "./styles";
+import { useState, useEffect } from "react";
+import Image from "next/image";
 
-const Banner = () => (
-  <S.Wrapper>
-    <S.LeftContent>
-      <S.Text>
-        Junte-se a nós para fazer a diferença na vida de quem mais precisa! Sua
-        doação pode transformar o futuro de muitas pessoas e comunidades
-        inteiras. Não perca a oportunidade de fazer o bem e ser parte dessa
-        corrente do bem. Cada centavo conta e cada gesto de solidariedade faz a
-        diferença. Vamos juntos mudar o mundo, uma doação de cada vez!
-      </S.Text>
-    </S.LeftContent>
-    <S.RightContent>
-      <Divider title={["rede"]} color="primary" />
-    </S.RightContent>
-  </S.Wrapper>
-);
+const Banner = () => {
+  const [fallLiquid, setFallLiquid] = useState(false);
+  const [animateLiquid, setAnimateLiquid] = useState(false);
 
+  useEffect(() => {
+    async function animationLiquid() {
+      setTimeout(() => setFallLiquid(true), 1000);
+      setTimeout(() => setAnimateLiquid(true), 2100);
+    }
+    animationLiquid();
+  }, []);
+  return (
+    <S.Wrapper>
+      <S.BorderMenu />
+      <S.LeftContent>
+        <S.Text>
+          Junte-se a nós para fazer a diferença na vida de quem mais precisa!
+          Sua doação pode transformar o futuro de muitas pessoas e comunidades
+          inteiras. Não perca a oportunidade de fazer o bem e ser parte dessa
+          corrente do bem. Cada centavo conta e cada gesto de solidariedade faz
+          a diferença. Vamos juntos mudar o mundo, uma doação de cada vez!
+        </S.Text>
+      </S.LeftContent>
+      <S.RightContent>
+        <S.BorderContent>
+          <S.Content>
+            <S.ImageWrapper>
+              <Image
+                src="/img/logo.png"
+                alt="Logo da RF reboque"
+                layout="fill"
+                objectFit="cover"
+              />
+            </S.ImageWrapper>
+            <S.TextContent>
+              <S.Title fallLiquid={fallLiquid} animateLiquid={animateLiquid}>
+                <span>A REDE</span>
+                <span>A REDE</span>
+              </S.Title>
+              <S.Title fallLiquid={fallLiquid} animateLiquid={animateLiquid}>
+                <span>SEMEANDO ESPERANÇA</span>
+                <span>SEMEANDO ESPERANÇA</span>
+              </S.Title>
+            </S.TextContent>
+          </S.Content>
+        </S.BorderContent>
+      </S.RightContent>
+    </S.Wrapper>
+  );
+};
 export default Banner;
--- src/components/Banner/styles.ts
+++ src/components/Banner/styles.ts
@@ -1,6 +1,5 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import media from "styled-media-query";
-import { BorderContent } from "../Divider/styles";
 
 export const Wrapper = styled.main`
   display: grid;
@@ -15,6 +14,7 @@ export const Wrapper = styled.main`
     rgba(255, 179, 0, 1) 100%
   );
   height: 100vh;
+  padding-top: 10rem;
 `;
 
 export const LeftContent = styled.div`
@@ -23,12 +23,7 @@ export const LeftContent = styled.div`
   align-items: center;
 `;
 
-export const RightContent = styled.div`
-  display: flex;
-  ${BorderContent} {
-    height: 80rem;
-  }
-`;
+export const RightContent = styled.div``;
 export const Text = styled.h4`
   font-size: 2.5rem;
   letter-spacing: 0.1rem;
@@ -36,24 +31,189 @@ export const Text = styled.h4`
   text-align: center;
 `;
 
-export const Title = styled.h2`
-  font-size: 7rem;
+export const ImageWrapper = styled.div`
+  width: 40rem;
+  height: 40rem;
   margin-top: -10rem;
-`;
-export const Subtitle = styled.h4`
-  font-size: 1.9rem;
-  letter-spacing: 0.1rem;
-  margin-top: -1.5rem;
+  ${media.lessThan("medium")`
+      width: 35rem;
+      margin-left: 1.5rem;
+      margin-bottom: 1.5rem;
+    `}
+  position: relative;
 `;
 
-export const Image = styled.div`
-  ${media.lessThan("medium")`
-    height: 35rem;
-    `}
-  height: 50rem;
-  width: 50rem;
-  background-image: url("/img/logo.png");
-  background-position: center;
-  background-size: cover;
-  margin: 0 5rem;
+export const TextContent = styled.div`
+  div:nth-child(1) {
+    margin-top: 1rem;
+    
+    font-size: 10rem;
+  }
+  div:nth-child(2) {
+    font-size: 3rem;
+  }
+`;
+export const BorderMenu = styled.div`
+  ${({ theme }) => css`
+    position: absolute;
+    height: 10.5rem;
+    margin-top: -10rem;
+    width: 100%;
+    z-index: 20;
+    background-color: ${theme.colors.primary};
+  `}
+`;
+
+export const Content = styled.div`
+  ${({ theme }) => css`
+    position: absolute;
+    z-index: 30;
+    top: 0;
+    left: 1rem;
+    right: 1rem;
+    bottom: 1rem;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    -webkit-clip-path: polygon(0 0, 100% 0, 75% 100%, 25% 100%);
+    clip-path: polygon(0 0, 100% 0, 75% 100%, 25% 100%);
+    background-color: ${theme.colors.black};
+    animation: fallDown 2s forwards;
+    overflow: hidden;
+    text-align: center;
+    @keyframes fallDown {
+      0% {
+        transform: translateY(-100%);
+      }
+      100% {
+        transform: translateY(0%);
+      }
+    }
+  `}
+`;
+
+export const BorderContent = styled.div`
+  ${({ theme }) => css`
+    margin-top: -0.5rem;
+    position: relative;
+    height: 80vh;
+    -webkit-clip-path: polygon(0 0, 100% 0, 75% 100%, 25% 100%);
+    clip-path: polygon(0 0, 100% 0, 75% 100%, 25% 100%);
+    animation: fallDown 2s forwards;
+    background: ${theme.colors.primary};
+    z-index: 30;
+    @keyframes fallDown {
+      0% {
+        transform: translateY(-100%);
+      }
+      100% {
+        transform: translateY(0%);
+      }
+    }
+  `}
+`;
+
+type TitleProps = {
+  fallLiquid: boolean;
+  animateLiquid: boolean;
+};
+
+export const Title = styled.div<TitleProps>`
+  ${({ theme, animateLiquid, fallLiquid }) => css`
+    margin-top: 5rem;
+    font-weight: 700;
+    max-width: 100rem;
+    margin-bottom: 8rem;
+    text-transform: uppercase;
+    span {
+      position: absolute;
+      color: #fff;
+      transform: translate(-50%, -50%);
+    }
+    span:nth-child(1) {
+      color: transparent;
+      -webkit-text-stroke: 0.1rem ${theme.colors.white};
+    }
+    span:nth-child(2) {
+      color: ${theme.colors.primary};
+      -webkit-text-stroke: 1px ${theme.colors.white};
+      clip-path: polygon(
+        0% 0%,
+        16% 0%,
+        34% 0%,
+        51% 0%,
+        67% 0%,
+        84% 0%,
+        100% 0%,
+        100% 60%,
+        0% 60%
+      );
+      ${fallLiquid &&
+      css`
+        animation: fallLiquids 1.2s ease-in-out forwards;
+      `}
+      ${animateLiquid &&
+      css`
+        animation: animation 2.5s ease-in-out infinite;
+      `}
+    }
+    @keyframes fallLiquids {
+      0% {
+        clip-path: polygon(
+          0% 0%,
+          16% 0%,
+          34% 0%,
+          51% 0%,
+          67% 0%,
+          84% 0%,
+          100% 0%,
+          100% 60%,
+          0% 60%
+        );
+      }
+      100% {
+        clip-path: polygon(
+          0% 45%,
+          15% 44%,
+          32% 50%,
+          54% 60%,
+          70% 61%,
+          84% 59%,
+          100% 52%,
+          100% 100%,
+          0% 100%
+        );
+      }
+    }
+    @keyframes animation {
+      0%,
+      100% {
+        clip-path: polygon(
+          0% 45%,
+          15% 44%,
+          32% 50%,
+          54% 60%,
+          70% 61%,
+          84% 59%,
+          100% 52%,
+          100% 100%,
+          0% 100%
+        );
+      }
+      50% {
+        clip-path: polygon(
+          0% 60%,
+          16% 65%,
+          34% 66%,
+          51% 62%,
+          67% 50%,
+          84% 55%,
+          100% 46%,
+          100% 100%,
+          0% 100%
+        );
+      }
+    }
+  `}
 `;
