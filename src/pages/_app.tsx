2c8072831a64b7ee0710671216ee33c8d2395529
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-03-29 19:53:17 -0300

    feat: adicionando titulo e descrição ao site

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ThemeProvider } from "styled-components";
 import type { AppProps } from "next/app";
+import Head from 'next/head'
 
 import theme from "@/styles/theme";
 import GlobalStyles from "@/styles/global";
@@ -7,6 +8,15 @@ import GlobalStyles from "@/styles/global";
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={theme}>
+       <Head>
+        <title>Projeto A Rede</title>
+        <meta name="theme-color" content="#191919" />
+        <link rel="shortcut icon" href="/img/logo.png" />
+        <meta
+          name="description"
+          content="Junte-se a nós para fazer a diferença na vida de quem mais precisa! "
+        />
+      </Head>
       <Component {...pageProps} />
       <GlobalStyles />
     </ThemeProvider>
