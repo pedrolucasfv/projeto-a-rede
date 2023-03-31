ca3017becbfe6fab662e9cdbf893077d54a321b1
pedrolucasfv <pedro.lucas.tukinha25@gmail.com>
2023-03-31 10:30:28 -0300

    feat: trocando o conteudo das oficinas

--- src/templates/Oficinas/index.tsx
+++ src/templates/Oficinas/index.tsx
@@ -27,8 +27,8 @@ const Oficinas = () => {
       />
       <OficinaContent
         name="Aulas de música"
-        description="Essa é uma oficina para ensinar inglês as crianças carentes para capacitá-las e promover um futuro melhor a elas. O inglês é de suma imporância e abre muitas portas para oportunidades de melhoria de vida."
-        image="/img/aula-de-canto-1.jpeg"
+        description="Essa é uma oficina para ensinar aulas de música para as crianças de 07 a 15 anos. A música é importante na formação de uma criança para desenvolver sua criatividade e coordenação motora. Além disso, a música promove a cultura, ajuda a descobrir um pontecial talento e cria momentos de descontração e alegria."
+        image="/img/aula-de-canto-2.jpeg"
         time="Terça-feira: 18:00 até 19:00"
         age="De 07 a 15 anos"
         invert
@@ -42,22 +42,22 @@ const Oficinas = () => {
       <S.OficinaCardContent>
         <OficinaCard
           name="Aulas de Jiu-Jitsu"
-          description="Essa é uma oficina para ensinar inglês as crianças carentes para capacitá-las e promover um futuro melhor a elas. O inglês é de suma imporância e abre muitas portas para oportunidades de melhoria de vida."
+          description="Essa é uma oficina para ensinar os alunos desenvolverem técnicas de defesa pessoal, exercícios de coordenação motora, disciplina e respeito. Além disso, a prática do Jiu Jitsu ajuda a desenvolver a confiança, autoestima e habilidades sociais das crianças, enquanto também as incentiva a adotar um estilo de vida mais ativo e saudável.  "
           image="/img/aula-de-jiujitsu.jpeg"
         />
         <OficinaCard
           name="Aulas de Informática"
-          description="Essa é uma oficina para ensinar inglês as crianças carentes para capacitá-las e promover um futuro melhor a elas. O inglês é de suma imporância e abre muitas portas para oportunidades de melhoria de vida."
+          description="Nessa oficina, as crianças aprendem sobre o uso do computador, seus componentes, sistemas operacionais, programas de software e a navegação na internet. Além disso, também podem aprender sobre programação básica e desenvolvimento de jogos simples. O objetivo dessas oficinas é fornecer às crianças um entendimento básico e prático da tecnologia, bem como incentivá-las a serem mais familiarizadas com o mundo digital."
           image="/img/aula-de-informatica.jpeg"
         />
          <OficinaCard
           name="Roda de Leitura"
-          description="Essa é uma oficina para ensinar inglês as crianças carentes para capacitá-las e promover um futuro melhor a elas. O inglês é de suma imporância e abre muitas portas para oportunidades de melhoria de vida."
+          description="Nessas oficinas, as crianças são convidadas a se sentarem em uma roda e a participarem de uma leitura coletiva de um livro escolhido. Durante a leitura, o professor pode fazer pausas para discutir os temas do livro, perguntar sobre os personagens e incentivar a reflexão sobre a história. O objetivo dessas oficinas é promover a leitura e o pensamento crítico das crianças, além de incentivá-las a expressar suas opiniões e ideias de forma criativa e colaborativa. "
           image="/img/roda-de-leitura.jpeg"
         />
          <OficinaCard
           name="Reforço Escolar"
-          description="Essa é uma oficina para ensinar inglês as crianças carentes para capacitá-las e promover um futuro melhor a elas. O inglês é de suma imporância e abre muitas portas para oportunidades de melhoria de vida."
+          description="Nessa oficina, as crianças recebem aulas extras, com o objetivo de complementar o conteúdo apresentado nas aulas regulares, sanar dúvidas e reforçar o aprendizado. As aulas podem ser individuais ou em grupo, dependendo da demanda e da necessidade de cada criança. O objetivo dessas oficinas é ajudar as crianças a superar as dificuldades escolares, melhorar o desempenho acadêmico e aumentar a autoestima e a confiança na sua capacidade de aprendizado."
           image="/img/reforco-escolar.jpeg"
         />
       </S.OficinaCardContent>
